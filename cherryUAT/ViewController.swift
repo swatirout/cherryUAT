946fabb53c20fedb39cf4af52d639cb61c18c912
Swati Rout <swatirout@MacBook-Pro.local>
2019-09-22 09:55:10 +0530

    Initial Commit

---
+++ cherryUAT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cherryUAT
+//
+//  Created by Swati Rout on 22/09/19.
+//  Copyright © 2019 Swati Rout. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
